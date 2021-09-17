1be7aa657f8dc2c9253903da578b79ef9296af22
p22rlugo <p22rlugo@fairfieldprep.org>
2021-09-17 10:10:37 -0400

    Add file

---
+++ lab_4-1.py
@@ -0,0 +1,14 @@
+# Ryan Lugo: RJL 9/17/21
+
+# Math Values
+a = 1 
+b = 2 
+c = 3 
+x = 2
+y = 5
+
+# Equation
+equation = ((3 + 4 * x) / 5) - ((10 * (y - 5) * (a + b + c)**2) / x) + 9 * (4 / x**2 + (9 + x) / y)
+
+# Print Statement
+print(equation)
