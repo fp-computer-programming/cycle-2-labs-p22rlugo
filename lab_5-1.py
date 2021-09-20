3d9ce1c58147c1ac55a307bb5c13c52930d83f96
p22rlugo <p22rlugo@fairfieldprep.org>
2021-09-20 10:03:55 -0400

    Add file

---
+++ lab_5-1.py
@@ -0,0 +1,36 @@
+# Ryan Lugo: RJL 9/20/21
+
+# Question 1
+# x+=5
+# x = 7
+x = x + 5
+
+# Question 2
+# y=y-3
+# y = 3
+y -= 3 
+
+# Question 3
+# z**=2
+# z = 9
+z = z**2
+
+# Question 4
+# w=w%2
+# w = 29
+w %= 2 
+
+# Question 5
+# t=t/5
+# t = 48
+t /= 5 
+
+# Question 6
+# a*=12
+# a = 3
+a = a * 12
+
+# Question 7
+# i=i//7
+# i = 5
+i //= 7
